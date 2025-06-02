ef0a50db241317e51eae0ef296f75342f3666e18
zoneme99 <jonatancervall@gmail.com>
2025-06-02 14:12:12 +0200

    cached in ask_price, maybe go parallelism?

--- custom_scripts/test.py
+++ custom_scripts/test.py
@@ -6,17 +6,25 @@ import json
 fio = FIO()
 
 # Get DW ticker information from all exchanges
-#material = fio.Exchange.get('DW.NC1')
+material = fio.Exchange.get('DW.NC1')
 #material = fio.Recipe.all()
 # Replace 'your_file.json' with the actual filename
 with open('custom_scripts/all_recipes.json', 'r') as f:
     data = json.load(f)
 
+Ask_price = dict()
 
 for recipe in data:
     for key in recipe.keys():
         for inputs in recipe['Inputs']:
-            print(inputs, fio.Exchange.get(f'{inputs['Ticker']}.NC1')['Ask'])
+            if inputs['Ticker'] in Ask_price.keys():
+                print(inputs, Ask_price[inputs['Ticker']])
+            else:
+                try:
+                    Ask_price[inputs['Ticker']] = fio.Exchange.get(f'{inputs['Ticker']}.NC1').Ask
+                    print(inputs, Ask_price[inputs['Ticker']])
+                except: print('error')
+
         #print(key, recipe[key])
     #break
                 #for ticker, quantity in dict2.values():
